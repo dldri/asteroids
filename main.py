2a24d34306815949a03ea3ef8323d5411544688b
dldri <diovian.liu@gmail.com>
2025-01-27 07:07:38 +0800

    added FPS components

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     # initialise pygame
     pygame.init()
 
+    # FPS components
+    clock = pygame.time.Clock() 
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -23,7 +27,7 @@ def main():
                 return
         screen.fill(colour)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
