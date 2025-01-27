7a822fe013acd12b51b6547a6eecd3a06e4d0c23
dldri <diovian.liu@gmail.com>
2025-01-27 18:04:02 +0800

    player rotation

--- main.py
+++ main.py
@@ -25,15 +25,16 @@ def main():
     y = SCREEN_HEIGHT / 2
     player_1 = Player(x, y)
 
-    colour = (0,0,0)
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(colour)
+        screen.fill("black")
         player_1.draw(screen)
+        player_1.update(dt)
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
